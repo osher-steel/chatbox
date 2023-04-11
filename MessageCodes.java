b919e015af004fe70483d532e6ec384b9b008373
osher-steel <111786194+osher-steel@users.noreply.github.com>
2023-04-11 14:13:13 -0400

    Create MessageCodes.java

---
+++ MessageCodes.java
@@ -0,0 +1,13 @@
+public class MessageCodes {
+    static final String SERVER_EXIT="server_exit";
+    static final String USER_EXIT="*exit";
+    static final String INIT_MESSAGE="init_message";
+    static final String NEW_CONNECTED="new_connected";
+    static final String USER_DISCONNECTED="user_disconnected";
+    static final String PRIVATE_MESSAGE="*private";
+    static final String ERROR_MESSAGE="*private";
+    static final String SERVER_CLOSED="server_closed";
+    static final String SERVER_OPEN="server_open";
+
+
+}
